16f75d3c88a0e862f8d10f9efaa73455c4e6c252
Benhur Külzer <benhur.bitpc@gmail.com>
2020-05-24 19:27:03 -0300

    Create Costumer Service and Repository

--- src/modules/customers/services/CreateCustomerService.ts
+++ src/modules/customers/services/CreateCustomerService.ts
@@ -12,10 +12,24 @@ interface IRequest {
 
 @injectable()
 class CreateCustomerService {
-  constructor(private customersRepository: ICustomersRepository) {}
+  constructor(
+    @inject('CustomerRepository')
+    private customersRepository: ICustomersRepository,
+  ) {}
 
   public async execute({ name, email }: IRequest): Promise<Customer> {
-    // TODO
+    const findEmail = await this.customersRepository.findByEmail(email);
+
+    if (findEmail) {
+      throw new AppError('This customer is already created');
+    }
+
+    const costumer = await this.customersRepository.create({
+      name,
+      email,
+    });
+
+    return costumer;
   }
 }
 
